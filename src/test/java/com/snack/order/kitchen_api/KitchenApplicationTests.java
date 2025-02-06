3a18bd7f8ecec6eca4579d25c160382cd90e02f0
kaua kapamadjian <kauakapamadjian@gmail.com>
2025-02-06 19:41:29 -0300

    first commit

---
+++ src/test/java/com/snack/order/kitchen_api/KitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snack.order.kitchen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
